2a79a14748810fad9b760dc865295e8224ca867b
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2017-11-10 15:44:06 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
+from setuptools import setup, find_packages
 setup(
     name='pyplfv',
     version='0.0.1',
     description='Python package for the Phase-locking statistical analysis of EEG data.',
-    long_description=readme,
+    long_description='README.md',
     author='Yoshimasa Sakuragi',
     author_email='ysakuragi16@gmail.com',
     install_requires=['numpy', 'pandas', 'scipy', 'matplotlib'],
     url='https://github.com/SakuragiYoshimasa/pyplfv',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
     test_suite='tests'
 )
