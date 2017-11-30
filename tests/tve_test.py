105523871db6d8324e8c127c0e58f438b0a25541
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2017-11-30 19:47:42 +0900

    comp tve sample

---
+++ tests/tve_test.py
@@ -0,0 +1,21 @@
+import numpy as np
+from pyplfv.tve import save_normalized_tve_with_farray
+from pyplfv.tve import save_tve_with_farray
+from pyplfv.utility import save_intermediate_data
+from pyplfv.utility import load_intermediate_data
+
+
+_waveleted_signal_with_farray = load_intermediate_data('./SampleData/simulation_data1_wavelet_wfa.pkl')
+
+#Save
+'''
+save_tve_with_farray(_waveleted_signal_with_farray, './SampleData/simulation_data1_tve_wfa.pkl')
+save_normalized_tve_with_farray(_waveleted_signal_with_farray, './SampleData/simulation_data1_ntve_wfa.pkl')
+'''
+
+#Load
+'''
+_tve_with_farray = load_intermediate_data('./SampleData/simulation_data1_tve_wfa.pkl')
+_normalized_tve_with_farray = load_intermediate_data('./SampleData/simulation_data1_ntve_wfa.pkl')
+print(_normalized_tve_with_farray)
+'''
